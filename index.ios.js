090c09537d4043f50e4cf19a758dba6654e18a50
Henry Doan <henrydoan96@gmail.com>
2016-11-09 15:55:44 -0700

    Stagger animations with animated stagger

--- index.ios.js
+++ index.ios.js
@@ -3,47 +3,55 @@ import {
   AppRegistry,
   StyleSheet,
   Animated,
+  Dimensions,
   Text,
   View
 } from 'react-native';
 
+const {height} = Dimensions.get("window");
 export default class AnimatedReactNative extends Component {
   
   componentWillMount() {
     this.animatedValue1 = new Animated.Value(0);
-    this.animatedValue2 = new Animated.Value(1);
+    this.animatedValue2 = new Animated.Value(0);
+    this.animatedValue3 = new Animated.Value(0);
   }
 
   componentDidMount() {
-    Animated.sequence([
+    Animated.stagger(300, [
       Animated.timing(this.animatedValue1, {
-        toValue: 150,
-        duration: 1000
+        toValue: height,
+        duration: 1500
       }),
-      Animated.spring(this.animatedValue2, {
-        toValue: 3,
+      Animated.timing(this.animatedValue2, {
+        toValue: height,
+        duration: 1500
       }),
-      Animated.timing(this.animatedValue1, {
-        toValue: 0,
-        duration: 1000
+      Animated.timing(this.animatedValue3, {
+        toValue: height,
+        duration: 1500
       }),
-      Animated.spring(this.animatedValue2, {
-        toValue: .5,
-      }),
-    ]).start()
+    ]).start();
   }
 
   render() {
 
-    const animatedStyle = {
-      transform: [
-        { translateY: this.animatedValue1 },
-        { scale: this.animatedValue2}
-      ]
+    const animatedStyle1 = {
+      height: this.animatedValue1
+    }
+    const animatedStyle2 = {
+      height: this.animatedValue2
+    }
+    const animatedStyle3 = {
+      height: this.animatedValue3
     }
     return (
       <View style={styles.container}>
-        <Animated.View style={[styles.box, animatedStyle]}>
+        <Animated.View style={[styles.box, animatedStyle1]}>
+        </Animated.View>
+        <Animated.View style={[styles.box, animatedStyle2]}>
+        </Animated.View>
+        <Animated.View style={[styles.box, animatedStyle3]}>
         </Animated.View>
       </View>
     );
@@ -53,16 +61,13 @@ export default class AnimatedReactNative extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'row',
     backgroundColor: '#F5FCFF',
   },
   box: {
+    flex: 1,
     backgroundColor: "#333",
-    width: 100,
-    height: 100,
-    justifyContent: 'center',
-    alignItems: "center",
+    marginHorizontal: 5,
   },
   button: {
     backgroundColor: "#333",
